212180f82882dc1b7cd348a926170311de80d435
Adalberto da Silva Maria <adalsilv@yahoo.com.br>
2019-06-23 07:23:36 -0300

    Publish Changes

--- FreedomApi/Program.cs
+++ FreedomApi/Program.cs
@@ -19,6 +19,7 @@ namespace FreedomApi
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>();
     }
 }
